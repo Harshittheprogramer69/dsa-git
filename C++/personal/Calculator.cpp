d511f03cfdb6ae5135612f282ec08c4c07e02d1a
Harshit_khanna <117803004+Gamer1312@users.noreply.github.com>
2026-01-08 18:09:52 +0530

    All C++ programs

---
+++ C++/personal/Calculator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op,ans;
+    char YON;
+    cin >> a;
+    cin >> op;
+    cin >> b; 
+    do {
+        switch (op) {
+            case '+': 
+            cout << (a + b) << endl;
+            break;
+            case '-':
+            cout << (a - b) << endl;
+            break;
+            case '*':
+            cout << (a * b) << endl;
+            break;
+            case '/':
+            if (b != 0)
+            cout << (a / b) << endl;
+            else
+            cout << "Division by zero error" << endl;
+            break;
+            default:
+            cout << "invalid response" << endl;
+        }
+        cout << "WRITE N";
+        cin >> YON;
+    } while (YON == "n" || YON == "N");
+    return 0;
+}
---
+++ C++/personal/printting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 12, y = 6;
+    string name = "harshit";
+    cout<<name<<". Roll number is: "<<x<<"and their number is :"<<y<<endl;
+    return 0;
+}
