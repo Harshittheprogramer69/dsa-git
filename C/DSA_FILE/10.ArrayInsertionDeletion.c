a03d51e74d8880d223f9a99713ee095e2f4f7225
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-01-16 21:35:24 +0530

    Changes in Insertion and deletion

--- C/DSA_FILE/10.ArrayInsertionDeletion.c
+++ C/DSA_FILE/10.ArrayInsertionDeletion.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int arr[50];
+    int array[50];
     int n, i;
-    int pos, value;
+    int position, value;
     int choice;
 
     printf("Enter number of elements: ");
@@ -12,7 +12,7 @@ int main() {
     printf("Enter elements:\n");
     for (i = 1; i <= n; i++) {
         printf("Enter Element[%d]: ",i);
-        scanf("%d", &arr[i]);
+        scanf("%d", &array[i]);
     }
 
     printf("Options are as Follows: \n");
@@ -21,35 +21,35 @@ int main() {
 
     if (choice == 1) {
         printf("Enter position to insert: ");
-        scanf("%d", &pos);
+        scanf("%d", &position);
 
         printf("Enter value: ");
         scanf("%d", &value);
 
-        for (i = n; i >= pos; i--) {
-            arr[i + 1] = arr[i];
+        for (i = n; i >= position; i--) {
+            array[i + 1] = array[i];
         }
 
-        arr[pos] = value;
+        array[position] = value;
         n++;
 
         printf("Array after insertion:\n");
         for (i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
+            printf("%d ", array[i]);
         }
     } else if (choice == 2) {
         printf("Enter position to delete: ");
-        scanf("%d", &pos);
+        scanf("%d", &position);
 
-        for (i = pos; i < n; i++) {
-            arr[i] = arr[i + 1];
+        for (i = position; i < n; i++) {
+            array[i] = array[i + 1];
         }
 
         n--;
 
         printf("Array after deletion:\n");
         for (i = 1; i <= n; i++) {
-            printf("%d ", arr[i]);
+            printf("%d ", array[i]);
         }
     } else {
         printf("Invalid choice");
