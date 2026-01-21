b35ef05bf71259315200f7d1315f3f9d27baf7c8
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-01-21 10:20:50 +0530

    CHANGES

---
+++ C++/E BalaguruSwamy/Chapter 2/PROGRAMMING EXERCISES/Question5.cpp
@@ -0,0 +1,15 @@
+/*
+2.5 Question : Write a C++ program that will ask for a temprature in Fahrenhiet and display it in celsius.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+            
+    
+    Fahrenite = ((9.0*Celsius)/5.0) + 32.0;
+
+
+    return 0;
+}
