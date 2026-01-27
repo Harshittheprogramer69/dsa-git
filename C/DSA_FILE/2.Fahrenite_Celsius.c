306b0fe962f9fb0a32ce45226988643b38acd7b9
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-01-27 11:07:28 +0530

    Spelling Fixed

--- C/DSA_FILE/2.Fahrenite_Celsius.c
+++ C/DSA_FILE/2.Fahrenite_Celsius.c
@@ -3,23 +3,23 @@
 int main() {
 
     int option;
-    float Celsius,Fahrenite;
+    float Celsius,Fahrenheit;
 
     printf("Enter What you want to do\n");
-    printf("1.Celsius to Fahrenite\n2.Fahrenite to Celsius\n");
+    printf("1.Celsius to Fahrenheit\n2.Fahrenheit to Celsius\n");
     
     scanf("%d",&option);
 
     if(option == 1) {
         printf("Enter Celsius: ");
         scanf("%f",&Celsius);
-        Fahrenite = ((9.0*Celsius)/5.0) + 32.0;
-        printf("%f Fahrenite is %f Celsius",Fahrenite,Celsius);
+        Fahrenheit = ((9.0*Celsius)/5.0) + 32.0;
+        printf("%f Fahrenheit is %f Celsius",Fahrenheit,Celsius);
     } else {
-        printf("Enter Fahrenite: ");
-        scanf("%f",&Fahrenite);
-        Celsius = (5.0*(Fahrenite - 32.0))/9.0;
-        printf("%f Celsius is %f Fahrenite",Celsius,Fahrenite);
+        printf("Enter Fahrenheit: ");
+        scanf("%f",&Fahrenheit);
+        Celsius = (5.0*(Fahrenheit - 32.0))/9.0;
+        printf("%f Celsius is %f Fahrenheit",Celsius,Fahrenheit);
 
     }
 
