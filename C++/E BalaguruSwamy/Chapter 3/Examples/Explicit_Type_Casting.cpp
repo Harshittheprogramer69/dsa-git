941e96538cdc837498e14683c1bce9a52200950b
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-02-15 10:13:26 +0530

    E BALGURUSWAMY Chapter 3 example 3.5

---
+++ C++/E BalaguruSwamy/Chapter 3/Examples/Explicit_Type_Casting.cpp
@@ -0,0 +1,15 @@
+// Example 3.5
+#include <iostream>
+using namespace std;
+
+int main() {
+    int intvar = 25;
+    float floatvar = 35.87;
+
+    cout << "intvar = " << intvar;
+    cout << "\nfloatvar = " << floatvar;
+    cout << "\nfloat(intvar) = " << float(intvar); 
+    cout << "\nint(floatvar) = " << int(floatvar);
+    
+    return 1;
+}
