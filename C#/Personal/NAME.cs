0b615fcb3d39d17f9401f256d7df57bc4f0e6746
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-02-18 09:59:14 +0530

    C#

---
+++ C#/Personal/NAME.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Name
+{
+    class Print_name
+    {
+        static void Main()
+        {
+            Console.Write("Enter your Name: ");
+            string name = (Console.ReadLine()!);
+
+            Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
